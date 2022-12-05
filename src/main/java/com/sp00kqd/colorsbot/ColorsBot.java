4344d9220c8a09a6f1d247bb272b30f18d8c8504
Sp00kqd <sp00kqd@gmail.com>
2022-12-05 00:16:18 -0500

    token stuff

--- src/main/java/com/sp00kqd/colorsbot/ColorsBot.java
+++ src/main/java/com/sp00kqd/colorsbot/ColorsBot.java
@@ -1,4 +1,37 @@
 package com.sp00kqd.colorsbot;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
 public class ColorsBot {
+
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    public ColorsBot() throws LoginException {
+        config = Dotenv.configure().ignoreIfMissing().load();
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createLight(config.get("TOKEN"));
+
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.streaming("click", "https://youtu.be/dQw4w9WgXcQ"));
+        shardManager = builder.build();
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ColorsBot bot = new ColorsBot();
+        } catch(LoginException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
